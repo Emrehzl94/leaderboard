eeb8486a60a10158bfbdf0b28118682a63e3af02
emreh <emrehizal_772@hotmail.com>
2020-06-15 23:29:25 +0300

    Swagger documentaion added.

---
+++ src/main/java/com/example/leaderboard/config/SwaggerConfig.java
@@ -0,0 +1,41 @@
+package com.example.leaderboard.config;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import static springfox.documentation.builders.PathSelectors.regex;
+
+@Configuration
+@EnableSwagger2
+@Log4j2
+public class SwaggerConfig implements WebMvcConfigurer {
+
+    {
+        log.info("Creating SwaggerConfig");
+    }
+
+    @Bean
+    public Docket clientApi() {
+        return new Docket(DocumentationType.SWAGGER_2).forCodeGeneration(true)
+                .select().apis(RequestHandlerSelectors.basePackage("com.example.leaderboard.controller"))
+                .paths(regex("/*.*"))
+                .build();
+
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+}
